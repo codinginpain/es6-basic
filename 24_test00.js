fe6bd91acfabb0bc9aeafb3dcb13960a44a28e29
codinginpain <51408335+codinginpain@users.noreply.github.com>
2020-03-08 23:38:42 +0900

    d

--- 24_test00.js
+++
@@ -1,9 +0,0 @@
-var http = require(`http`);
-
-var server = http.createServer();
-
-var host = `192.168.1.115`;
-var port = 3000;
-server.listen(port, host, 50000, function() {
-    console.log(`웹 서버 실행.`);
-});
